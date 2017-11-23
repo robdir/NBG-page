a6f384578bd8a46de68eddef06ba99410366bcf4
robdir <rhwickham@gmail.com>
2017-11-23 14:40:51 +0100

    conflict really resolved

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,7 @@ import React, { PureComponent } from 'react'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
 import ExpandMore from 'material-ui/svg-icons/navigation/expand-more'
-<<<<<<< HEAD
-import FlatButton from 'material-ui/FlatButton'
-=======
 import TextField from 'material-ui/TextField'
->>>>>>> 493e8d7914d3c6e377fce73929f02d712ba8468a
 
 
 class Navigation extends PureComponent {
@@ -16,11 +12,7 @@ class Navigation extends PureComponent {
       <AppBar
         title="NationaleBeroepenGids.nl"
         iconElementLeft={<IconButton ><ExpandMore /></IconButton>}
-<<<<<<< HEAD
-        iconElementRight={<FlatButton label="Uw NBG" />}
-=======
         iconElementRight={<TextField hintText="Zoeken naar..."/>}
->>>>>>> 493e8d7914d3c6e377fce73929f02d712ba8468a
         />
     )
   }
