3acbcd33272db0de83b797d5938028ecc20a41ae
mrjln <marjolein_tromp@hotmail.com>
2017-11-23 14:31:46 +0100

    navbar and banner

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
 import ExpandMore from 'material-ui/svg-icons/navigation/expand-more'
-
+import TextField from 'material-ui/TextField'
 
 
 class Navigation extends PureComponent {
@@ -10,8 +10,9 @@ class Navigation extends PureComponent {
   render() {
     return (
       <AppBar
-        title="nationaleberoepengids.nl"
+        title="NationaleBeroepenGids.nl"
         iconElementLeft={<IconButton ><ExpandMore /></IconButton>}
+        iconElementRight={<TextField hintText="Zoeken naar..."/>}
         />
     )
   }
