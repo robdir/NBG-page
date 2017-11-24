87740bb97e03fb201e2397d5df56b4902501cb40
mrjln <marjolein_tromp@hotmail.com>
2017-11-24 11:49:49 +0100

    added organisation component

---
+++ src/components/Organisation.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './styles/Container_styles.css'
+
+class Organisation extends Component {
+
+  render() {
+    return(
+        <div className="container description">
+        <h2>Organisaties waar een webdeveloper doorgaans werkzaam is </h2>
+        <hr/>
+          <div><p> Een Webdeveloper kan bij vele soorten bedrijven aan de slag
+           zolang die maar een website hebben die ontwikkeld moet worden en
+           onderhoud nodig heeft. Dit kan zowel bij een groot als een klein
+           bedrijf, een particulier of publiek bedrijf, en op freelance
+          of vaste basis. Vaak komt men dan op de communicatie- of marketingafdeling
+           van de organisatie terecht. </p></div>
+      )
+    }
+}
+
+export default Organisation
