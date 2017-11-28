f90a70c1ab7d136aa28a49f5fe2c3a86616365ac
robdir <rhwickham@gmail.com>
2017-11-28 10:39:59 +0100

    small nav change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import Menu from 'material-ui/svg-icons/navigation/menu'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import muiTheme from '../assets/styles/theme';
 import PropTypes from 'prop-types'
 
@@ -30,13 +29,12 @@ class Sidebar extends PureComponent {
           label="Toggle Drawer"
           onClick={this.Open}
         />
-        <MuiThemeProvider muiTheme={muiTheme}>
         <Drawer open={this.state.open}>
           <MenuItem><p>Ga direct naar:</p></MenuItem>
           <hr/>
           <MenuItem onClick={this.Close}><a href='#Omschrijving'><p>Functieomschrijving</p></a></MenuItem>
-          <MenuItem onClick={this.Close}><a href='#Vacatures'><p>Vacatures</p></a></MenuItem>
           <MenuItem onClick={this.Close}><a href='#Opleiding'><p>Opleiding</p></a></MenuItem>
+          <MenuItem onClick={this.Close}><a href='#Vacatures'><p>Vacatures</p></a></MenuItem>
           <MenuItem onClick={this.Close}><a href='#Salaris'><p>Salaris</p></a></MenuItem>
           <MenuItem onClick={this.Close}><a href='#Netwerk'><p>Netwerk</p></a></MenuItem>
           <MenuItem onClick={this.Close}><a href='#Persoontypen'><p>Persoontypen</p></a></MenuItem>
@@ -45,7 +43,6 @@ class Sidebar extends PureComponent {
           <hr/>
           <MenuItem onClick={this.Close}> Menu sluiten </MenuItem>
         </Drawer>
-        </MuiThemeProvider>
       </div>
     );
   }
