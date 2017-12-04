524bddfd658ddc685147804da6811fa3fad8a93b
robdir <rhwickham@gmail.com>
2017-12-04 11:41:01 +0100

    handle close on esc

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,12 +13,11 @@ class Sidebar extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {open: false};
-    this.Open = this.Open.bind(this);
-    this.Close = this.Close.bind(this);
   }
 
-  Open = () => this.setState({open: true});
-  Close = () => this.setState({open: false});
+  handleOpen = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
 
   render() {
     return (
@@ -26,25 +25,27 @@ class Sidebar extends PureComponent {
         <Menu
           style={{ height: "40px", color: "White"}}
           label="Toggle Drawer"
-          onClick={this.Open}
+          onClick={this.handleOpen}
         />
-        <Drawer containerStyle={menuStyle}
+        <Drawer
+          containerStyle={menuStyle}
+          docked={false}
           open={this.state.open}
-          onRequestChange={this.Close}
-          docked={false} >
+          onRequestChange={(open) => this.setState({open})}
+        >
           <div className="menu">
           <MenuItem><p>Ga direct naar:</p></MenuItem>
           <hr/>
-          <MenuItem onClick={this.Close}><a href='#Omschrijving'><p>Functieomschrijving</p></a></MenuItem>
-          <MenuItem onClick={this.Close}><a href='#Opleiding'><p>Opleiding</p></a></MenuItem>
-          <MenuItem onClick={this.Close}><a href='#Vacatures'><p>Vacatures</p></a></MenuItem>
-          <MenuItem onClick={this.Close}><a href='#Salaris'><p>Salaris</p></a></MenuItem>
-          <MenuItem onClick={this.Close}><a href='#Netwerk'><p>Netwerk</p></a></MenuItem>
-          <MenuItem onClick={this.Close}><a href='#Persoontypen'><p>Persoontypen</p></a></MenuItem>
-          <MenuItem onClick={this.Close}><a href='#Organisatie'><p>Organisaties</p></a></MenuItem>
-          <MenuItem onClick={this.Close}><a href='#Interview'><p>Interview</p></a></MenuItem>
+          <MenuItem onClick={this.handleClose}><a href='#Omschrijving'><p>Functieomschrijving</p></a></MenuItem>
+          <MenuItem onClick={this.handleClose}><a href='#Opleiding'><p>Opleiding</p></a></MenuItem>
+          <MenuItem onClick={this.handleClose}><a href='#Vacatures'><p>Vacatures</p></a></MenuItem>
+          <MenuItem onClick={this.handleClose}><a href='#Salaris'><p>Salaris</p></a></MenuItem>
+          <MenuItem onClick={this.handleClose}><a href='#Netwerk'><p>Netwerk</p></a></MenuItem>
+          <MenuItem onClick={this.handleClose}><a href='#Persoontypen'><p>Persoontypen</p></a></MenuItem>
+          <MenuItem onClick={this.handleClose}><a href='#Organisatie'><p>Organisaties</p></a></MenuItem>
+          <MenuItem onClick={this.handleClose}><a href='#Interview'><p>Interview</p></a></MenuItem>
           <hr/>
-          <MenuItem onClick={this.Close}> <p> Menu sluiten </p> </MenuItem>
+          <MenuItem onClick={this.handleClose}> <p> Menu sluiten </p> </MenuItem>
           </div>
         </Drawer>
       </div>
